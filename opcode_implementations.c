444104c02d1c8ea2db1c3ca110d6285edc6bc81a
waweroh <scadmsez@gmail.com>
2021-12-02 07:20:06 -0800

    resolved betty style compliance

--- opcode_implementations.c
+++ opcode_implementations.c
@@ -12,10 +12,8 @@ void op_push(stack_t **stack, __attribute__((unused)) unsigned int line_number)
 	stack_t *node = NULL;
 
 	node = malloc(sizeof(stack_t));
-	/*printf("Before malloc in push function\n");*/
 	if (node == NULL)
 	{
-		/*write(STDERR_FILENO, "Error: malloc failed\n", 22);*/
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
@@ -23,24 +21,16 @@ void op_push(stack_t **stack, __attribute__((unused)) unsigned int line_number)
 	node->n = push_arg;
 	node->prev = NULL;
 
-	/*printf("After malloc, before if stack is NULL statement\n");*/
-
 	if (*stack == NULL)
 	{
 		node->next = NULL;
 		*stack = node;
-		/*printf("Before return in if stack is NULL statement\n");*/
 		return;
 	}
-	/*printf("After if stack is NULL statement\n");*/
 	node->next = *stack;
-	/*printf("After node->next = *stack statement\n");
-	  printf("After node->next = *stack statement\n");*/
-	/*printf("After node->next = *stack statement\n");*/
+
 	(*stack)->prev = node;
-	/*printf("After stack->prev = node statement\n");*/
 	*stack = node;
-	/*printf("At end of push function\n");*/
 }
 
 /**
@@ -59,11 +49,9 @@ void op_pall(stack_t **stack, __attribute__((unused)) unsigned int line_number)
 	tmp = *stack;
 	while (tmp)
 	{
-		/*printf("Before pall\n");*/
 		printf("%d\n", tmp->n);
 		tmp = tmp->next;
 	}
-	/*printf("After pall loop\n");*/
 }
 
 /**
