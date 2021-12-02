9062539b3616c96dd6512a2d1eb23ae3efdfd6b9
Washington Obungu <tobungu@gmail.com>
2021-12-02 03:31:46 -0800

    Updated header file by resolving merge conflicts

--- monty.h
+++ monty.h
@@ -1,25 +1,17 @@
-<<<<<<< HEAD
 #ifndef MONTY_H
 #define MONTY_H
 
 #define  _GNU_SOURCE
-=======
-#ifndef _MONTY_H_
-#define _MONTY_H_
 
->>>>>>> 40f27ee7e314f0ed77bb4694f0f52584d97861ea
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-<<<<<<< HEAD
 #include <fcntl.h>
+#include <ctype.h>
 
 /* Integer argument to the push opcode variable declaration*/
 int push_arg;
-=======
-#include <ctype.h>
->>>>>>> 40f27ee7e314f0ed77bb4694f0f52584d97861ea
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -27,24 +19,14 @@ int push_arg;
  * @prev: points to the previous element of the stack (or queue)
  * @next: points to the next element of the stack (or queue)
  *
-<<<<<<< HEAD
  * Description: doubly linked list structure
-=======
- * Description: doubly linked list node structure
->>>>>>> 40f27ee7e314f0ed77bb4694f0f52584d97861ea
  * for stack, queues, LIFO, FIFO
  */
 typedef struct stack_s
 {
-<<<<<<< HEAD
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
-=======
         int n;
         struct stack_s *prev;
         struct stack_s *next;
->>>>>>> 40f27ee7e314f0ed77bb4694f0f52584d97861ea
 } stack_t;
 
 /**
@@ -53,7 +35,6 @@ typedef struct stack_s
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
-<<<<<<< HEAD
  * for stack, queue, LIFO, FIFO
  */
 typedef struct instruction_s
@@ -73,22 +54,3 @@ void op_pall(stack_t **stack, unsigned int line_number);
 void exec_instruction(char *command, stack_t **stack, unsigned int line_number);
 
 #endif /* !MONTY_H */
-=======
- * for stack, queues, LIFO, FIFO
- */
-typedef struct instruction_s
-{
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
-} instruction_t;
-
-void push(stack_t **stack, unsigned int line_number);
-void pall(stack_t **stack, unsigned int line_number);
-void pint(stack_t **stack, unsigned int line_number);
-void pop(stack_t **stack, unsigned int line_number);
-void swap(stack_t **stack, unsigned int line_number);
-void add(stack_t **stack, unsigned int line_number);
-void nop(stack_t **stack, unsigned int line_number);
-
-#endif
->>>>>>> 40f27ee7e314f0ed77bb4694f0f52584d97861ea
