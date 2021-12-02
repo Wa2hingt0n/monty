491f30d866e1627d751d633b747d17a27878654d
waweroh <scadmsez@gmail.com>
2021-12-02 07:21:07 -0800

    resolved betty style compliance

--- string_tokenizing.c
+++ string_tokenizing.c
@@ -3,7 +3,7 @@
 /**
  * _strtok - Tokenizes a string
  * @str: String to be tokenized
- *
+ * @line_number: line number of opcode.
  * Return: Pointer to an array of tokenized strings
  */
 char *_strtok(char *str, unsigned int line_number)
@@ -14,15 +14,9 @@ char *_strtok(char *str, unsigned int line_number)
 
 	tokens = strtok(str, " \t\n");
 	if (tokens == NULL)
-		return NULL;
+		return (NULL);
 
-	/*argv = malloc(sizeof(char *) * argv_size);
-	if (argv == NULL)
-	{
-		write(STDERR_FILENO, "Error: malloc failed\n", 22);
-		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}*/
+
 
 	argv = tokens;
 	tokens = strtok(NULL, " \n\t");
@@ -33,22 +27,10 @@ char *_strtok(char *str, unsigned int line_number)
 	}
 	else if (tokens == NULL && strcmp(argv, "push") == 0)
 	{
-		/*write(STDERR_FILENO, "Usage: push integer\n", 21);*/
 		fprintf(stderr, "L%u: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
-	/*while (tokens != NULL && i < 2)
-	{
-		argv[i] = tokens;
-		if (i == 1)
-		{
-			argv[i] = atoi(argv[i]);
-		}
-		tokens = strtok(NULL, " \t\n");
-		i++;
-		}*/
-	/*argv[i] = NULL;*/
 
 	return (argv);
 }
